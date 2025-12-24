77f0bd23197ed4d972d0a21c1ba05ad3869d2a00
Prapitesh <116258736+Prapitesh@users.noreply.github.com>
2025-12-24 19:23:11 +0530

    Change API base URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:7777",
+  baseURL: "https://library-backend-2-xuej.onrender.com",
 });
 
 /* 🔐 Attach BASIC AUTH correctly */
